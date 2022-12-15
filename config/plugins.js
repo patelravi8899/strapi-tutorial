83c62ead6bf04d0ee77c698b46e8c7051f236044
patelravi8899 <ravi.patel@mobiosolutions.com>
2022-12-15 13:50:49 +0530

    backend

---
+++ config/plugins.js
@@ -0,0 +1,22 @@
+module.exports = ({ env }) => ({
+    // ...
+    email: {
+      config: {
+        provider: 'nodemailer',
+        providerOptions: {
+          host: env('SMTP_HOST', 'smtp.example.com'),
+          port: env('SMTP_PORT', 587),  
+          auth: {
+            user: env('SMTP_USERNAME'),
+            pass: env('SMTP_PASSWORD'),
+          },
+          // ... any custom nodemailer options
+        },
+        settings: {
+          defaultFrom:  env('EMAIL_ADDRESS_FROM'),
+          defaultReplyTo:  env('EMAIL_ADDRESS_REPLY'),
+        },
+      },
+    },
+    // ...
+  });
